b1e59fdd0f35f069b34e7b1d88357288ac76faca
Artem Maslii <maslijartei@gmail.com>
2024-04-10 18:25:12 +0300

    Updated Router settings and added layout component

--- src/components/customRouterProvider/CustomRouterProvider.tsx
+++ src/components/customRouterProvider/CustomRouterProvider.tsx
@@ -1,7 +1,8 @@
-import { Suspense, FC, lazy } from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { ROUTE_PATHS } from "../../constants/routePaths";
-import Loader from "../shared/Loader";
+import { Suspense, FC, lazy } from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { ROUTE_PATHS } from '../../constants/routePaths';
+import Loader from '../shared/Loader';
+import Layout from '../layout/Layout';
 
 const Home = lazy(() => import('../../pages/home/Home'));
 const CategoriesPage = lazy(() => import('../../pages/categories/CategoriesPage'));
@@ -9,47 +10,52 @@ const CategoryPage = lazy(() => import('../../pages/categories/CategoryPage'));
 const CommentsList = lazy(() => import('../../pages/comments/CommentsList'));
 
 const CustomRouterProvider: FC = () => {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Layout />,
+      children: [
+        {
+          path: ROUTE_PATHS.HOME,
+          element: (
+            <Suspense fallback={<Loader />}>
+              <Home />
+            </Suspense>
+          ),
+        },
+        {
+          path: ROUTE_PATHS.CATEGORIES,
+          element: (
+            <Suspense fallback={<Loader />}>
+              <CategoriesPage />
+            </Suspense>
+          ),
+        },
+        {
+          path: ROUTE_PATHS.CATEGORY,
+          element: (
+            <Suspense fallback={<Loader />}>
+              <CategoryPage />
+            </Suspense>
+          ),
+        },
+        {
+          path: ROUTE_PATHS.COMMENTS,
+          element: (
+            <Suspense fallback={<Loader />}>
+              <CommentsList />
+            </Suspense>
+          ),
+        },
+      ],
+    },
+  ]);
 
-    const router = createBrowserRouter([
-        {
-            path: ROUTE_PATHS.HOME,
-            element: (
-                <Suspense fallback={<Loader />}>
-                    <Home />
-                </Suspense>
-            ),
-        },
-        {
-            path: ROUTE_PATHS.CATEGORIES,
-            element: (
-                <Suspense fallback={<Loader />}>
-                    <CategoriesPage />
-                </Suspense>
-            ),
-        },
-        {
-            path: ROUTE_PATHS.CATEGORY,
-            element: (
-                <Suspense fallback={<Loader />}>
-                    <CategoryPage />
-                </Suspense>
-            ),
-        },
-        {
-            path: ROUTE_PATHS.COMMENTS,
-            element: (
-                <Suspense fallback={<Loader />}>
-                    <CommentsList />
-                </Suspense>
-            ),
-        }
-    ]);
-
-    return (
-        <>
-            <RouterProvider router={router} />
-        </>
-    );
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 };
 
 export default CustomRouterProvider;
