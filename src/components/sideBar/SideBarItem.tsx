f6afdb2da625cfd4b97be5969e81dd2a5a96813d
Artem Maslii <maslijartei@gmail.com>
2024-04-12 23:28:24 +0300

    Change of color

--- src/components/sideBar/SideBarItem.tsx
+++ src/components/sideBar/SideBarItem.tsx
@@ -20,15 +20,15 @@ const SidebarItem: React.FC<SidebarItemProps> = ({ text, icon, path }) => {
         flexDirection: 'column',
         alignItems: 'center',
         textAlign: 'center',
-        color: match ? 'primary.main' : 'inherit',
+        color: match ? 'primary.main' : '#000048',
         '&:hover': {
           backgroundColor: 'rgba(0, 0, 0, 0.04)',
         },
       }}
     >
-      <IconButton sx={{ color: match ? 'primary.main' : 'action.active' }}>
+      <IconButton sx={{ color: match ? 'primary.main' : '#000048' }}>
         {React.cloneElement(icon, {
-          color: match ? 'primary' : 'inherit',
+          color: match ? 'primary' : '#000048',
         })}
       </IconButton>
       <Typography variant="body2" sx={{ mt: 0.5 }}>
