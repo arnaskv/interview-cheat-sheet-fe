b1138b4b32bfa56c44ef91af1b6ee4f081df0425
kasparas.putrius <kasparas.putrius@gmail.com>
2024-04-19 21:10:03 +0300

    Made some fixes to layout: - added to header and footer to layout so that it can wrap content in the Box(basically main page content like questions and categories), Thus removing both header and footer from index.tsx - Fixed footer to stay at the bottom of the view port, no matter window size and page content.

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,14 +1,17 @@
+import React from 'react';
 import styles from './Footer.module.css';
 
 const Footer = () => {
   return (
-    <footer className={styles.FooterBox}>
-      <div className={styles.Copyright}>Copyright © 2024 Cognizant</div>
-      <div className={styles.PrivacyPolicy}>
-        <a href="https://www.cognizant.com/us/en/about-cognizant/public-policy">Privacy Policy</a>{' '}
-        {/* Should be a link to this page's privacy policy */}
-      </div>
-    </footer>
+    <div className={styles.FooterContainer}>
+      <footer className={styles.FooterBox}>
+        <div className={styles.Copyright}>Copyright © 2024 Cognizant</div>
+        <div className={styles.PrivacyPolicy}>
+          <a href="https://www.cognizant.com/us/en/about-cognizant/public-policy">Privacy Policy</a>{' '}
+          {/* Should be a link to this page's privacy policy */}
+        </div>
+      </footer>
+    </div>
   );
 };
 
