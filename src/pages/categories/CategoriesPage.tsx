f42bc1c7c4c7014bd9d01b2450782f6f271b4be9
Tomas Miliūnas <tomasmiliunas14@gmail.com>
2024-04-25 20:08:50 +0300

    Removed meaningless comment

--- src/pages/categories/CategoriesPage.tsx
+++ src/pages/categories/CategoriesPage.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Categories from '../../components/categories/Categories';
-//import styles from '../../components/categories/Categories.module.css';
 
 const CategoriesPage: React.FC = () => {
   return (
