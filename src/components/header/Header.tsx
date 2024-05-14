42e2a39a4b46a87e23fda3197e2b47bc6818414a
Paulius Preikša <preiksap@gmail.com>
2024-05-14 13:27:45 +0300

    feat: added user icon with navigation

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,15 +1,60 @@
+import { IconButton, Menu, MenuItem } from '@mui/material';
 import styles from './Header.module.css';
 // import NotificationsIcon from '@mui/icons-material/Notifications';
-// import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import { UserContext } from '../authenticationProvider/AuthenticationProvider';
+import { useContext, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
+
+  const navigate = useNavigate();
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = (event: any) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const user = useContext(UserContext);
+
     return (
       <div className={styles.HeaderContainer}>
         <header className={styles.HeaderBox}>
-            {/*<div className={styles.ButtonsBox}>*/}
+            <div className={styles.ButtonsBox}>
             {/*    <NotificationsIcon className={styles.Notification}/>*/}
-            {/*    <AccountCircleIcon className={styles.Profile}/>*/}
-            {/*</div>*/}
+            <IconButton className={styles.Profile} onClick={handleClick}>
+              <AccountCircleIcon className={styles.ProfileButton} />
+            </IconButton>
+            <Menu
+              id="profile-menu"
+              anchorEl={anchorEl}
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+            >
+              {user?.isLoggedIn ? (
+                <MenuItem onClick={() => {
+                  user.logout();
+                  navigate('/');
+                  handleClose();
+                }}>Log out</MenuItem>
+              ) : (
+                <>
+                  <MenuItem onClick={() => {
+                    navigate('/login');
+                    handleClose();
+                  }}>Log in</MenuItem>
+                  <MenuItem onClick={() => {
+                      navigate('/register')
+                      handleClose();
+                  }}>Register</MenuItem>
+                </>
+              )}
+            </Menu>
+            </div>
         </header>
       </div>
     );
