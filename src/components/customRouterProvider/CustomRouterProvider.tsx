1f0041974da73e32d1eb0cfa30af50fdcf5be7df
Artem Maslii <maslijartei@gmail.com>
2024-05-27 17:29:32 +0300

    Refactor: lazy import for not found page in router

--- src/components/customRouterProvider/CustomRouterProvider.tsx
+++ src/components/customRouterProvider/CustomRouterProvider.tsx
@@ -3,8 +3,8 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { ROUTE_PATHS } from '../../constants/routePaths';
 import Loader from '../shared/Loader';
 import Layout from '../layout/Layout';
-import NotFoundPage from '../shared/NotFoundPage';
 
+const NotFoundPage = lazy(() => import('../shared/NotFoundPage'));
 const Home = lazy(() => import('../../pages/home/Home'));
 const CategoriesPage = lazy(() => import('../../pages/categories/CategoriesPage'));
 
